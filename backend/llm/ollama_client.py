0906399496390b04acca70a681d6dc65169bd03c
ronver1 <ronitver1@hotmail.com>
2026-06-10 19:06:27 -0500

    Initial rebuilding of test file inside backend

---
+++ backend/llm/ollama_client.py
@@ -0,0 +1,8 @@
+# Author: Ronit Verma
+# Created on: 6.10.26
+
+# This file consists of a function that takes in a user message, sends it to Ollama, and returns 
+# the chatbot's response
+
+# Logic: No inputs, wait for user input, send user input to chatbot, then save response as a variable, 
+# return that variable
